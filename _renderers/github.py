21a5cad132524cda5929d5aca6906a7dc517e71f
Paul Traylor <kungfudiscomonkey@gmail.com>
2022-09-04 14:23:16 +0900

    Add support for mqtt_subscribe
    In order to reuse most of our code for other projects, we want to special
    case salt/engines/hook where we need to parse the incoming payload. For
    other types of events, we're going to assume that the data is already in
    the correct format and does not need extra parsing.
    
    This will allow other projects like https://github.com/kfdm/salt-mqtt to
    deploy the correct payload directly into Salt's bus and not worry about
    extra special casing there.

--- _renderers/github.py
+++ _renderers/github.py
@@ -11,17 +11,32 @@ __opts__ = {}
 log = logging.getLogger(__name__)
 
 
-def render(dict_data, saltenv="", sls="", **kwargs):
+def render(dict_data, saltenv="", sls="", tag="", **kwargs):
     event = salt.utils.event.get_master_event(
         __opts__, __opts__["sock_dir"], listen=False
     )
     lowstate = {}
-    body = json.loads(kwargs["data"]["body"])
-    headers = kwargs["data"]["headers"]
 
-    if headers["X-Github-Event"] != "push":
-        log.warning("Skipping %s event", headers["X-Github-Event"])
-        return lowstate
+    log.debug("Rendering for %s", tag)
+    # If our tag is from the engine hook, then we will need to parse
+    # the data and pull out the body and headers
+    if tag.startswith("salt/engines/hook"):
+        assert "body" in kwargs["data"], "Webhook payload should have body"
+        assert "headers" in kwargs["data"], "Webhook payload should have headers"
+        body = json.loads(kwargs["data"]["body"])
+        headers = kwargs["data"]["headers"]
+
+        if headers["X-Github-Event"] != "push":
+            log.warning("Skipping %s event", headers["X-Github-Event"])
+            return lowstate
+    # Otherwise, we'll assume that the data came from another source
+    # and will try to process the body as is. This allows us to support other
+    # sources like mqtt_subscribe engine
+    else:
+        body = kwargs["data"]
+
+    assert "repository" in body, "Payload should have repository object"
+    assert "ref" in body, "Payload should have git ref"
 
     try:
         event.fire_event(
